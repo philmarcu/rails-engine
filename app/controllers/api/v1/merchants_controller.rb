dc06227c1d524a1e0e073951d76934c3a363e1df
philmarcu <philmark18@gmail.com>
2022-08-30 13:05:13 -0600

    Feat - added merchants create action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,21 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  def create
+    merchant = Merchant.new(merc_params)
+    if merchant.save
+      render json: MerchantSerializer.new(merchant), status: 201
+    end
+  end
+  
+  private
+
+  def merc_params
+    params.require(:merchant).permit(:name)
+  end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,15 +10,43 @@ RSpec.describe 'Merchants API' do
     expect(response.status).to eq(200)
 
     parsed_json = JSON.parse(response.body, symbolize_names: true)
-
     merchants = parsed_json[:data]
+
+    expect(merchants.size).to eq(3)
 
     merchants.each do |m|
       expect(m).to have_key(:id)
       expect(m[:id]).to be_a(String)
       
       expect(m).to have_key(:attributes)
+      expect(m[:type]).to eq("merchant")
       expect(m[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can get a specific merchant' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+    merchant = parsed_json[:data]
+
+    expect(merchant[:id]).to be_a(String)
+    expect(merchant[:attributes][:name]).to be_a(String)
+  end
+
+  it 'can create a new merchant' do
+    merc_params = ({
+      name: Faker::Name.name
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merc_params)
+    created_merc = Merchant.last
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(created_merc.name).to eq(merc_params[:name])
   end
 end
